228df49e02bb8fa6537fbe82eeab2baeb54c2cf5
ramose <leopapilaya@gmail.com>
2024-11-21 16:25:57 +0800

    Show signin state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,76 +1,122 @@
-'use client'
+"use client";
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import {
-	signInWithGoogle,
-	signOut,
-	onAuthStateChanged
+  signInWithGoogle,
+  signOut,
+  onAuthStateChanged,
 } from "@/src/lib/firebase/auth.js";
 import { addFakeRestaurantsAndReviews } from "@/src/lib/firebase/firestore.js";
 import { useRouter } from "next/navigation";
 import { firebaseConfig } from "@/src/lib/firebase/config";
 
 function useUserSession(initialUser) {
-	return;
+  return;
 }
 
-export default function Header({initialUser}) {
+export default function Header({ initialUser }) {
+  function useUserSession(initialUser) {
+    // The initialUser comes from the server via a server component
+    const [user, setUser] = useState(initialUser);
+    const router = useRouter();
 
-	const user = useUserSession(initialUser) ;
+    // Register the service worker that sends auth state back to server
+    // The service worker is built with npm run build-service-worker
+    useEffect(() => {
+      if ("serviceWorker" in navigator) {
+        const serializedFirebaseConfig = encodeURIComponent(
+          JSON.stringify(firebaseConfig)
+        );
+        const serviceWorkerUrl = `/auth-service-worker.js?firebaseConfig=${serializedFirebaseConfig}`;
 
-	const handleSignOut = event => {
-		event.preventDefault();
-		signOut();
-	};
+        navigator.serviceWorker
+          .register(serviceWorkerUrl)
+          .then((registration) =>
+            console.log("scope is: ", registration.scope)
+          );
+      }
+    }, []);
 
-	const handleSignIn = event => {
-		event.preventDefault();
-		signInWithGoogle();
-	};
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged((authUser) => {
+        setUser(authUser);
+      });
 
-	return (
-		<header>
-			<Link href="/" className="logo">
-				<img src="/friendly-eats.svg" alt="FriendlyEats" />
-				Friendly Eats
-			</Link>
-			{user ? (
-				<>
-					<div className="profile">
-						<p>
-							<img className="profileImage" src={user.photoURL || "/profile.svg"} alt={user.email} />
-							{user.displayName}
-						</p>
+      return () => unsubscribe();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-						<div className="menu">
-							...
-							<ul>
-								<li>{user.displayName}</li>
+    useEffect(() => {
+      onAuthStateChanged((authUser) => {
+        if (user === undefined) return;
 
-								<li>
-									<a
-										href="#"
-										onClick={addFakeRestaurantsAndReviews}
-									>
-										Add sample restaurants
-									</a>
-								</li>
+        // refresh when user changed to ease testing
+        if (user?.email !== authUser?.email) {
+          router.refresh();
+        }
+      });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user]);
 
-								<li>
-									<a href="#" onClick={handleSignOut}>
-										Sign Out
-									</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</>
-			) : (
-				<div className="profile"><a href="#" onClick={handleSignIn}>
-					<img src="/profile.svg" alt="A placeholder user image" />
-					Sign In with Google
-				</a></div>
-			)}
-		</header>
-	);
+    return user;
+  }
+
+  const handleSignOut = (event) => {
+    event.preventDefault();
+    signOut();
+  };
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    signInWithGoogle();
+  };
+
+  return (
+    <header>
+      <Link href="/" className="logo">
+        <img src="/friendly-eats.svg" alt="FriendlyEats" />
+        Friendly Eats
+      </Link>
+      {user ? (
+        <>
+          <div className="profile">
+            <p>
+              <img
+                className="profileImage"
+                src={user.photoURL || "/profile.svg"}
+                alt={user.email}
+              />
+              {user.displayName}
+            </p>
+
+            <div className="menu">
+              ...
+              <ul>
+                <li>{user.displayName}</li>
+
+                <li>
+                  <a href="#" onClick={addFakeRestaurantsAndReviews}>
+                    Add sample restaurants
+                  </a>
+                </li>
+
+                <li>
+                  <a href="#" onClick={handleSignOut}>
+                    Sign Out
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="profile">
+          <a href="#" onClick={handleSignIn}>
+            <img src="/profile.svg" alt="A placeholder user image" />
+            Sign In with Google
+          </a>
+        </div>
+      )}
+    </header>
+  );
 }
